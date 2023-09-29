3ae141c1eb18e80cb89b46543ac8dcdcef8fb431
HéctorVG <hectorvalenzuela.alu@iespacomolla.es>
2023-09-29 20:15:34 +0200

    Ejercicio 231bola8

--- src/ejercicios/231bola8.php
+++ src/ejercicios/231bola8.php
@@ -1,1 +1,7 @@
 <?php
+    $respuestas = ["Sí", "No", "Quizás", "Claro que sí", "Por supuesto que no", "No lo tengo claro", "Seguro que sí", "Yo diría que sí", "Ni de coña nano"];
+
+    $pregunta = $_POST["pregunta"];
+
+    print_r("<b>".$pregunta."</b>");
+    print_r("<br>".$respuestas[random_int(0,8)]);
